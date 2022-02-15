fc7c92b5cdf1bff8fe6cc9590506c3dd9329e57d
Daigorex14 <Koonx12@gmail.com>
2022-02-15 16:32:22 +0700

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  promotion = [
+    {
+      title : "White Album",
+      subtitle: "40th Macross",
+      image:"https://macross.jp/wp-content/uploads/Cybersecurity_header_0201-scaled.jpg",
+      detail:"40周年イヤーに突入し、先月『MACROSS7 BASARA EXPLOSION 2022』、4月に『ワルキューレ LIVE 2022』を控え、勢いがとまらない「マクロス」と、サイバーセキュリティに対する取組を重点的に推進する国の「2022年サイバーセキュリティ月間」とのコラボレーションが決定！"
+    },
+    {
+      title : "MACROSS DELTA",
+      subtitle: "LIVE 2022 〜Walküre Reborn!〜 ",
+      image:"https://macross.jp/wp-content/uploads/202110071809_27643_2.png",
+      detail:"いよいよ明日2021年10月8日（金）に完全新作『劇場版マクロスΔ 絶対LIVE!!!!!!』（同時上映『劇場短編マクロスF ～時の迷宮～』）がＴＯＨＯシネマズ 池袋、ほか全国ロードショー、来週10月13日にはワルキューレ3rdアルバム「Walküre Reborn!」発売と勢いがとまらない「マクロス」シリーズ。"
+    },
+    {
+      title : "M魂",
+      subtitle: "SUPER DIMENSION FANCLUB",
+      image:"https://macross.jp/wp-content/uploads/2021/12/bnr_fanclub_l-1.jpg",
+      detail:"超時空ファンクラブ「M魂」では、シリーズ第一作「超時空要塞マクロス」から「マクロスΔ」までのシリーズ全てのファンの方々に向けたサービスを展開していきます。オリジナル会員証の発行と特典プレゼントはもちろん、イベントチケットの優先受付など会員ならではのサービスが楽しめる年額プランと、「M魂」のメールマガジンやオンラインストアをご利用いただける無料プランの2つがございます。"
+    },
+  ];
+
   constructor() {}
+
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,50 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  news = [
+    {
+      title : "White Album",
+      subtitle: "40th Macross",
+      image:"https://pocgamer.files.wordpress.com/2014/07/un_spacy_by_n1nj4_katak_chan.png",
+    },{
+      title : "MACROSS DELTA",
+      subtitle: "LIVE 2022 〜Walküre Reborn!〜 ",
+      image:"https://pocgamer.files.wordpress.com/2014/07/un_spacy_by_n1nj4_katak_chan.png",
+    },{
+      title : "M魂",
+      subtitle: "SUPER DIMENSION FANCLUB",
+      image:"https://pocgamer.files.wordpress.com/2014/07/un_spacy_by_n1nj4_katak_chan.png",
+    },{
+      title : "White Album",
+      subtitle: "40th Macross",
+      image:"https://pocgamer.files.wordpress.com/2014/07/un_spacy_by_n1nj4_katak_chan.png",
+    },{
+      title : "MACROSS DELTA",
+      subtitle: "LIVE 2022 〜Walküre Reborn!〜 ",
+      image:"https://pocgamer.files.wordpress.com/2014/07/un_spacy_by_n1nj4_katak_chan.png",
+    },{
+      title : "M魂",
+      subtitle: "SUPER DIMENSION FANCLUB",
+      image:"https://pocgamer.files.wordpress.com/2014/07/un_spacy_by_n1nj4_katak_chan.png",
+    },{
+      title : "White Album",
+      subtitle: "40th Macross",
+      image:"https://pocgamer.files.wordpress.com/2014/07/un_spacy_by_n1nj4_katak_chan.png",
+    },{
+      title : "MACROSS DELTA",
+      subtitle: "LIVE 2022 〜Walküre Reborn!〜 ",
+      image:"https://pocgamer.files.wordpress.com/2014/07/un_spacy_by_n1nj4_katak_chan.png",
+    },{
+      title : "M魂",
+      subtitle: "SUPER DIMENSION FANCLUB",
+      image:"https://pocgamer.files.wordpress.com/2014/07/un_spacy_by_n1nj4_katak_chan.png",
+    },{
+      title : "M魂",
+      subtitle: "SUPER DIMENSION FANCLUB",
+      image:"https://pocgamer.files.wordpress.com/2014/07/un_spacy_by_n1nj4_katak_chan.png",
+    }
+  ];
+
   constructor() {}
 
 }
